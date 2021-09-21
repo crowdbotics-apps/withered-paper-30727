27e618aecdee1df6c1f18ae33a324239367feadb
Crowdbotics <team@crowdbotics.com>
2021-09-21 18:37:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_paper_30727Identifier",
+  url: "https://withered_paper_30727Identifier.botics.co",
+  api: "https://withered_paper_30727Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
